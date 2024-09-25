30343f993d4d557dfb18c4419b61bd0eb5331e9c
c4thyc <cathyc22@nycstudents.net>
2024-09-25 16:14:23 +0000

    updated

--- Feeder.java
+++ Feeder.java
@@ -6,6 +6,20 @@ public class Feeder {
      * in the constructor and always greater than or equal to zero
      */
     private int currentFood;
+    
+    public Feeder() {};
+    
+
+    public Feeder(int c)
+    {
+        currentFood = c;
+    }
+
+    public int getCurrentFood()
+    {
+        return currentFood;
+    }
+
 
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
@@ -13,6 +27,7 @@ public class Feeder {
      */
     public void simulateOneDay(int numBirds)
     {
+        if(Math.random() < 0.05) currentFood = 0;
     }
 
     /**
--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
+
+
 public class Main
 {
-
-public static void main(String[] args)
-  {
-    Feeder f = new Feeder();
-    
-  }
-  
-}  
-    
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        // System.out.println(f.currentFood);
+        System.out.println(f.getCurrentFood());
+        Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+        int random = (int)(Math.random() * 40 + 10);
+        System.out.println(random);
+        System.out.println(Math.random() < 0.05);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+    }
+}
