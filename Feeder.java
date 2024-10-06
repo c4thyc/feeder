2bbc72823f57b2db69573958775ada2e4f014a52
c4thyc <cathyc22@nycstudents.net>
2024-10-06 01:34:01 +0000

    updated

--- Feeder.java
+++ Feeder.java
@@ -50,7 +50,7 @@ public class Feeder {
     {
         int days = 0;
 
-        while(numDays > 0);
+        while(numDays > 0)
         {
             if(currentFood == 0) return days;
             simulateOneDay(numBirds);
--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
-
-
 public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
+        // Feeder f = new Feeder();
         // System.out.println(f.currentFood);
         // System.out.println(f.getCurrentFood());
         Feeder g = new Feeder(500);
         // System.out.println(g.getCurrentFood());
-        int random = (int)(Math.random() * 40 + 10);
+        // int random = (int)(Math.random() * 40 + 10);
         // System.out.println(random);
         // System.out.println(Math.random() < 0.05);
         g.simulateOneDay(12);
@@ -20,7 +18,11 @@ public class Main
         Feeder i = new Feeder(100);
         i.simulateOneDay(5);
         // System.out.println(i);
-        Feeder j = new Feeder(2400);
-        System.out.println(j.simulateManyDays(10, 4));
+        // Feeder j = new Feeder(2400);
+        // System.out.println(j.simulateManyDays(10, 4));
+        // Feeder k = new Feeder(250);
+        // System.out.println(k.simulateManyDays(10, 5));
+        Feeder l = new Feeder(0);
+        System.out.println(l.simulateManyDays(5, 10));
     }
 }
